86c1d02abb4956b2d534afe10909fbdff0f9a5d3
Md.Sabbir Rahman Saun <sabbirrahman3586@gmail.com>
2025-02-04 21:38:56 +0600

    task added

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log('hello world');
+// task no: 01;
+
+var taka =1000;
+var orange = 700;
+var apple = 190;
+var lemon = 80;
+
+var total = orange + apple + lemon;
+var khorsTk = total;
+var  currentTk = taka-khorsTk;
+console.log(currentTk);
+
+// output = 30tk
+
+// task no : 02;
+
+var totalSub = 5 ;
+var math = 70.5;
+var bangla = 95.3;
+var english = 54.3
+var chemistry = 70 ;
+var ict = 50 ;
+
+var totalMarks = math + bangla + english + chemistry + ict;
+var currentMarks = totalMarks ;
+var averageMarks = currentMarks / totalSub ;
+var fixedMarks = averageMarks.toFixed(2)
+var floatMarks = parseFloat(fixedMarks)
+console.log(  floatMarks)
+// output : 68.02;
+
+// task no : o3;
+var number = 500;
+var divideNum = number / 5;
+ console.log(divideNum)
+//  output : 100;
+
+
+//  task no : 04;
+var a = isNaN ('11');
+console.log(a)
+
+var b = isNaN(2-10);
+console.log(b)
+
+// output : false and false
+
